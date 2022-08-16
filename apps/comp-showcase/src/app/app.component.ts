0863a2c179f864ad5fb5a0cc01e585daec73f5c7
Leonardo Martins <leonardo.martins@projuris.com.br>
2022-08-16 11:40:30 -0300

    initial commit

---
+++ apps/comp-showcase/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lm-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'comp-showcase';
+}
